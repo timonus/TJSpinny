4c7c8c810bb3f71e4b5e4bc9b216922419719f64
Tim Johnsen <timonus@gmail.com>
2021-06-28 20:00:01 -0700

    Use blurs.

--- UIViewController+Loading.m
+++ UIViewController+Loading.m
@@ -34,35 +34,37 @@ static char *const kLoadingIndicatorKey = "kLIK";
     if (self.isViewLoaded && !objc_getAssociatedObject(self, kLoadingIndicatorKey)) {
         static const CGFloat kLoadingViewSideLength = 125.0;
         static const CGFloat kLoadingViewCornerRadius = 12.0;
-        
-        UIView *loadingView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, kLoadingViewSideLength, kLoadingViewSideLength)];
+
+        UIBlurEffectStyle blurStyle;
+        if (@available(iOS 13.0, *)) {
+            blurStyle = UIBlurEffectStyleSystemMaterialDark;
+        } else {
+            blurStyle = UIBlurEffectStyleDark;
+        }
+        UIBlurEffect *effect = [UIBlurEffect effectWithStyle:blurStyle];
+        UIVisualEffectView *loadingView = [[UIVisualEffectView alloc] initWithEffect:effect];
+        loadingView.bounds = CGRectMake(0.0, 0.0, kLoadingViewSideLength, kLoadingViewSideLength);
         loadingView.userInteractionEnabled = NO;
         loadingView.layer.cornerRadius = kLoadingViewCornerRadius;
+        loadingView.clipsToBounds = YES;
         loadingView.center = CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds));
+        loadingView.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.3];
         objc_setAssociatedObject(self, kLoadingIndicatorKey, loadingView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         loadingView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
         [view addSubview:loadingView];
         
-        static const CGFloat kLoadViewLightInterfaceBackgroundAlpha = 0.8;
-        
         UIActivityIndicatorViewStyle style;
-        
+        UIVisualEffect *vibrancyEffect;
 #if !defined(__IPHONE_13_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_13_0
         if (@available(iOS 13.0, *)) {
 #endif
             style = UIActivityIndicatorViewStyleLarge;
-            loadingView.backgroundColor = [UIColor colorWithDynamicProvider:^UIColor * _Nonnull(UITraitCollection * _Nonnull traitCollection) {
-                if (traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
-                    return [UIColor colorWithWhite:0.0 alpha:kLoadViewLightInterfaceBackgroundAlpha];
-                } else {
-                    return [UIColor tertiarySystemBackgroundColor];
-                }
-            }];
             loadingView.layer.cornerCurve = kCACornerCurveContinuous;
+            vibrancyEffect = [UIVibrancyEffect effectForBlurEffect:effect style:UIVibrancyEffectStyleLabel];
 #if !defined(__IPHONE_13_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_13_0
         } else {
             style = UIActivityIndicatorViewStyleWhiteLarge;
-            loadingView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:kLoadViewLightInterfaceBackgroundAlpha];
+            vibrancyEffect = [UIVibrancyEffect effectForBlurEffect:effect];
         }
 #endif
         UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:style];
@@ -71,7 +73,7 @@ static char *const kLoadingIndicatorKey = "kLIK";
         activityIndicator.center = CGPointMake(CGRectGetMidX(loadingView.bounds), CGRectGetMidY(loadingView.bounds));
         activityIndicator.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
         [activityIndicator startAnimating];
-        [loadingView addSubview:activityIndicator];
+        [loadingView.contentView addSubview:activityIndicator];
         
         _setActivityIndicatorViewHidden(loadingView, YES);
         [UIView animateWithDuration:0.25
