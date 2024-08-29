d8271de8f98fca28c7a5540f73f67ae36b66867c
Tim Johnsen <timonus@gmail.com>
2024-08-29 16:54:53 -0700

    Add SPM support.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "TJSpinny",
+    platforms: [.iOS(.v12), .tvOS(.v13), .macCatalyst(.v13)],
+    products: [
+        .library(
+            name: "TJSpinny",
+            targets: ["TJSpinny"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TJSpinny",
+            path: ".",
+            sources: ["UIViewController+Loading.m"],
+            publicHeadersPath: "."
+        )
+    ]
+)
